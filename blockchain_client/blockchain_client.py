eb8d38704610d3fed82486965a2fceaa75c42a5d
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-08-08 23:08:02 -0400

    Composed the logic to generate the signature using PKCS-v1.5 Public Key Crypto Standards, to sign the hashed transaction details

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request
 import Crypto
 from Crypto import Random
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5  #
+from Crypto.Hash import SHA
 import binascii
 from collections import OrderedDict
 
@@ -22,6 +24,21 @@ class Transaction:
             'amount': self.amount,
         })
 
+    def sign_transaction(self):
+        private_key = RSA.import_key(binascii.unhexlify(self.sender_private_key))
+        # Inverting the hexadecimal conversion of the sender private key
+        # RSA.importKey is used to get the key
+        signer = PKCS1_v1_5.new(private_key)  # Create a signature using sender_private_key using a PKCS algorithm
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        # Step 1: self.to_dict() => Dict version of transaction info(sender+recipient keys and amount)
+        # Step 2: str() => converts the dict into a string
+        # Step 3: encoding the obtained string in UTF-8 encoding format
+        # Step 4: the encoded string of transaction details is hashed using SHA hashing algorithm
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
+        # Step 1: Sign the generated hash of the transaction details with the signature created from sender_private_key
+        # Step 2: Convert the Signed hash of the transaction details into Hexadecimal format
+        # Step 3: Return the Signed Hash, decoded into ASCII
+
 
 app = Flask(__name__)
 
@@ -29,6 +46,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('./index.html')
+
 
 # API to generate a new transaction
 @app.route('/transaction/generate', methods=['POST'])
@@ -43,12 +61,10 @@ def generate_transactions():
     transaction = Transaction(sender_public_key, sender_private_key, recipient_public_key, amount)
 
     # Step 3: Return the response, which is a Dictionary
-    response = {
-        'transaction': transaction.to_dict(), # Converting the transantion details into a transaction
-        'signature': 'blah'
-    }
+    response = {'transaction': transaction.to_dict(),  # Converting the transaction details into a transaction dict
+                'signature': transaction.sign_transaction()}
 
-    return jsonify(response), 200 # Converting the Dict to JSON
+    return jsonify(response), 200  # Converting the Dict to JSON
 
 
 # Page to create transaction
@@ -67,18 +83,19 @@ def view_transactions():
 @app.route('/wallet/new')
 def new_wallet():
     random_gen = Crypto.Random.new().read
-    private_key = RSA.generate(1024, random_gen) # Generating a Private key using RSA Algo
+    private_key = RSA.generate(1024, random_gen)  # Generating a Private key using RSA Algo
     # 1st param => Key Length or Key size in bits _must be atleast 1024_ || 2nd param => Function that returns random-
     # -bytes _Crypto.Random()is used here_
-    public_key = private_key.publickey() # Generating a Public key from the Private key
+    public_key = private_key.publickey()  # Generating a Public key from the Private key
 
-    response = {    # Dictionary
-        'private_key' : binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+    response = {  # Dictionary
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
         # exporting the key in DER encoded format and converting it to hexadecimal
-        'public_key' : binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
     }
 
-    return jsonify(response), 200 # The response will be a Dictionary, so converting it into JSON, so the browser can understand
+    return jsonify(
+        response), 200  # The response will be a Dictionary, so converting it into JSON, so the browser can understand
 
 
 if __name__ == '__main__':
