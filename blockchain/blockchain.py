c5906eda89af79cb224b174e4933819bea688695
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-08-09 23:01:32 -0400

    Composed the feature to retrieve the pending transactions, that are to be added to a new block and display it in a table

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -7,11 +7,13 @@ from Crypto.Signature import PKCS1_v1_5  #
 from Crypto.Hash import SHA
 import binascii
 
+MINING_SENDER = "The Blockchain"
+
 
 class Blockchain:
 
     def __init__(self):
-        self.transactions = []
+        self.pending_transactions = []
         self.chain = []
         # Create the Genesis Block
         self.create_block(0, '00')
@@ -21,13 +23,13 @@ class Blockchain:
         block = {
             'block_number': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions': self.transactions,
+            'pending_transactions': self.pending_transactions,
             'nonce': nonce,
             'previous_hash': previous_hash
         }
 
-        # Reset the list of transactions after adding the pending transaction to the latest created block
-        self.transactions = []
+        # Reset the list of pending_transactions after adding the pending transaction to the latest created block
+        self.pending_transactions = []
         self.chain.append(block)
 
     def verify_signature(self, sender_public_key, signature, transaction):
@@ -37,19 +39,20 @@ class Blockchain:
         # Get the transaction => convert it into string => encode it to UTF-8 => Hash is using SHA
         h = SHA.new(str(transaction).encode('utf8'))
         # Now verify that the signature has already been done by/with the public_key
-        return verifier.verify(h, binascii.unhexlify(signature))
+        # verify does not return True or False, rather raise a value error
+        try:
+            verifier.verify(h, binascii.unhexlify(signature))
+            return True  # Return True if everything is good
+        except ValueError:
+            return False  # Return False if verify raises a ValueError
         # This will return true, if the Hash corresponds to the signature, which was signed by/using this public key
 
         # When an information is signed, it will be signed using the private key, but for verification, it done using-
         # -the public key of the signer
 
-
     def submit_transaction(self, sender_public_key, recipient_public_key, signature, amount):
         # Fn() to add the new transaction to pending transactions
 
-        # => Reward the miner
-
-        # => Signature Validation
         transaction = OrderedDict({
             'sender_public_key': sender_public_key,
             'recipient_public_key': recipient_public_key,
@@ -57,15 +60,23 @@ class Blockchain:
             # The transaction should be verified only with Sender&Recipient public key + amount
             'amount': amount
         })
-        signature_verification = self.verify_signature(sender_public_key, signature, transaction) # Temp var
-        if signature_verification:  # If the signature is verified to be true
-            self.transactions.append(transaction)  # Append the transaction into the current transaction
-            return len(self.chain) + 1  # The "+ 1" is to intimate the user that this transaction will be added to the-
-            # -next block
-        else:
-            return False  # Return False if the signature is not Authentic
 
-        return True
+        # => Reward the miner
+        # To reward the miner, the sender_public_key should be specific or else, the block was mined from another-
+        # -transaction
+        if sender_public_key == MINING_SENDER: # Reward for Mining a Block
+            self.pending_transactions.append(transaction)  # Append the transaction into the current transaction list without-
+            # -any checking, as the transaction is from the Blockchain
+            return len(self.chain) + 1
+        else:  # Transaction from one wallet to another
+            # => Signature Validation
+            signature_verification = self.verify_signature(sender_public_key, signature, transaction) # Temp var
+            if signature_verification:  # If the signature is verified to be true
+                self.pending_transactions.append(transaction)  # Append the transaction into the current transaction
+                return len(self.chain) + 1 # The "+ 1" intimates the client that this transaction will be added to-
+                # -the next block
+            else:
+                return False  # Return False if the signature is not Authentic
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
@@ -80,11 +91,31 @@ def index():
     return render_template('./index.html')
 
 
-@app.route('/transaction/new', methods=['POST'])
+@app.route('/transactions/get', methods=['GET'])
+def get_transactions():
+    pending_transactions = blockchain.pending_transactions
+    response = {'pending_transactions': pending_transactions}
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.form
 
     # Check the required fields for values
+    required = [
+                'confirmation_sender_public_key',
+                'confirmation_recipient_public_key',
+                'transaction_signature',
+                'confirmation_amount'
+               ]
+    if not all( k in values for k in required):
+        return 'Missing Values', 400
+
+    # This is entirely from the backend, so the form may not be relied upon completely, for the transaction details
+    # This logic just checks to see if everything is in place correctly, also prevents any vulnerability like when-
+    # -an attacker tries to send a form => This is just a prototype
+
     transaction_results = blockchain.submit_transaction(
                                                         values['confirmation_sender_public_key'],
                                                         values['confirmation_recipient_public_key'],
